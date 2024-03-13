5ba63af8c1648d8bbff54e9fcdc06cd8c1000909
dinh777 <you@example.comdduong4@students.avc.edu>
2024-03-13 12:13:44 -0700

    commit

--- main.hpp
+++ main.hpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// void swapValues(int &, int &);
-// void swapValues(int &, int &, int &);
-// void swapValues(int &, int &, int &, int &);
-
 void swapValues(int &n1, int &n2){
 
     int placeholder = n2;
